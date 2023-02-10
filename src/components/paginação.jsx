8185d990186ce95100be157f647e267b28dc2824
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-02-10 17:44:01 -0300

    ainda tentando fazer a paginação

--- src/components/paginação.jsx
+++ src/components/paginação.jsx
@@ -1,46 +1,28 @@
-import React, { useState, useEffect } from "react";
-import Pagination from "./Pagination";
+import React from "react";
+import { useState } from "react";
 
-const MainComponent = () => {
-  const [items, setItems] = useState([]);
-  const [totalPages, setTotalPages] = useState(0);
-  const pageSize = 10;
+import ReactPaginate from "react-paginate";
 
-  useEffect(() => {
-    // Fetch items from an API
-    const fetchData = async () => {
-      const result = await fetch("https://api.example.com/items");
-      const items = await result.json();
-      setItems(items);
-      setTotalPages(Math.ceil(items.length / pageSize));
-    };
-    fetchData();
-  }, []);
+function MyComponent() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
 
   return (
-    <div>
-      <h1>List of Items</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.slice(0, pageSize).map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <Pagination totalPages={totalPages} pageSize={pageSize} />
-    </div>
+    <ReactPaginate
+      previousLabel={"previous"}
+      nextLabel={"next"}
+      breakLabel={"..."}
+      breakClassName={"break-me"}
+      pageCount={10}
+      marginPagesDisplayed={2}
+      pageRangeDisplayed={5}
+      onPageChange={handlePageClick}
+      containerClassName={"pagination"}
+      subContainerClassName={"pages pagination"}
+      activeClassName={"active"}
+    />
   );
-};
-
-export default MainComponent;
+}
