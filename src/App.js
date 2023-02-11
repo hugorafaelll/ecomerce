154fb96fe482a63a9f7a1147ca4671ef64aff7b8
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-02-11 12:41:28 -0300

    tentando aplicar o redux a aplicação

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import { Container } from "@material-ui/core";
 import Exemplo from "./components/Exemplo";
-import CardPadrao from "./components/CardFirstColums";
 import FirsSection from "./components/FirsSection";
+import { Provider } from "react-redux";
+import store from "./components/redux/store";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <Container maxWidth="xl">
-      <Router>
-        <Header />
-      </Router>
+    <Provider store={store}>
+      <Container maxWidth="xl">
+        <Router>
+          <Header />
+        </Router>
 
-      <Exemplo />
-      <FirsSection />
-    </Container>
+        <Exemplo />
+        <FirsSection />
+      </Container>
+    </Provider>
   );
 };
 
---
+++ src/components/redux/reducers/productReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  products: [],
+};
+
+export const productReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      return state;
+    default:
+      return state;
+  }
+};
