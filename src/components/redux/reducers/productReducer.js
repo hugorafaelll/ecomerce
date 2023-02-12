db0051aaa9b953cae17ca4abded517b6e6ad6b87
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-02-12 18:39:50 -0300

    aprendendo redux

--- src/components/redux/reducers/productReducer.js
+++ src/components/redux/reducers/productReducer.js
@@ -1,8 +1,16 @@
+import { ActionTypes } from "../constants/action-types";
+
 const initialState = {
-  products: [],
+  products: [
+    {
+      id: 1,
+      title: "Primeiro REDUX",
+      category: "programer",
+    },
+  ],
 };
 
-export const productReducer = (state, { type, payload }) => {
+export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
       return state;
