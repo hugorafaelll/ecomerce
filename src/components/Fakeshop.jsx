f7de324ba34bbfded1769d505eca675a39477dc6
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2023-02-15 16:35:26 -0300

    ainda se matando com redux

--- src/components/Fakeshop.jsx
+++ src/components/Fakeshop.jsx
@@ -12,9 +12,14 @@ import Fab from "@mui/material/Fab";
 import Favorite from "@material-ui/icons/Favorite";
 import BasicModal from "./BasicModal";
 
+//redux
+import { add, remove } from "../redux/cartSlice.js";
+import { useDispatch } from "react-redux";
+
 const Fakeshop = ({ loja }) => {
   const [products, setProducts] = useState([]);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const dispatch = useDispatch();
 
   const url = "https://fakestoreapi.com/products";
 
@@ -34,6 +39,10 @@ const Fakeshop = ({ loja }) => {
 
   const closeModal = () => {
     setSelectedProduct(null);
+  };
+
+  const handleAdd = (item) => {
+    dispatch(add(item));
   };
 
   return (
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice.js";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
+  enhancers: [composeWithDevTools()],
 });
 
 export default store;
